a1cd968044dae546e3a4ac6e6ebe6c190327f203
Aleksandr Smelik <suntrope55@gmail.com>
2025-02-22 12:26:09 +0600

    fix: redis host

--- src/core/settings.py
+++ src/core/settings.py
@@ -57,9 +57,9 @@ class Settings(BaseSettings):
 
 class Cache(BaseSettings):
     model_config = config
-    CACHE_HOST: str = "0.0.0.0"
-    CACHE_PORT: int = 6379
-    CACHE_DB: int = 0
+    REDIS_HOST: str = "0.0.0.0"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
 
 
 class DB(BaseSettings):
