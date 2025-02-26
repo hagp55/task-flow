763b0b80240c009ca2b932215787a23448da20be
Aleksandr Smelik <suntrope55@gmail.com>
2025-02-26 17:10:41 +0600

    add tests for create users

--- tests/fixtures/infrastructure.py
+++ tests/fixtures/infrastructure.py
@@ -1,16 +1,29 @@
 import pytest
+from httpx import ASGITransport, AsyncClient
 
-from src.core.db import Base
-from tests.db_connector import TestingSessionLocal, engine
+from src.core.db import Base, engine
+from tests.db_connector import TestingSessionLocal, app, test_engine
+
+TEST_DB_NAME = "test_task_db"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def init_database():
+    async with engine.begin() as conn:
+        await conn.exec_driver_sql(f"DROP DATABASE IF EXISTS {TEST_DB_NAME} WITH (FORCE)")
+        await conn.exec_driver_sql(f"CREATE DATABASE {TEST_DB_NAME}")
+    yield
+    async with engine.begin() as conn:
+        await conn.exec_driver_sql(f"DROP DATABASE IF EXISTS {TEST_DB_NAME} WITH (FORCE)")
 
 
 @pytest.fixture(scope="function", autouse=True)
 async def init_tables():
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
@@ -18,3 +31,12 @@ async def init_tables():
 async def session():
     async with TestingSessionLocal() as session:
         yield session
+
+
+@pytest.fixture(scope="session")
+async def async_client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as async_client:
+        yield async_client
---
+++ tests/integration/users/test_create_users.py
@@ -0,0 +1,91 @@
+import pytest
+from fastapi import status
+from httpx import AsyncClient, Response
+
+
+@pytest.mark.parametrize(
+    "email,password,first_name,last_name",
+    [
+        ("valid@domain.com", "password", None, None),
+        ("valid@domain.com", "password", "John", None),
+        ("valid@domain.com", "password", "John", "Doe"),
+    ],
+)
+@pytest.mark.integration
+async def test_create_user__success(
+    email: str,
+    password: str,
+    first_name: str | None,
+    last_name: str | None,
+    async_client: AsyncClient,
+) -> None:
+    response: Response = await async_client.post(
+        "/api/v1/users/",
+        json={
+            "email": email,
+            "password": password,
+            "firstName": first_name,
+            "lastName": last_name,
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.integration
+async def test_create_exists_user__fail(async_client: AsyncClient) -> None:
+    response: Response = await async_client.post(
+        "/api/v1/users/",
+        json={
+            "email": "beazley@example.com",
+            "password": "Str0ngP@ssw0rd!",
+            "firstName": "test_user",
+            "lastName": "test_user",
+        },
+    )
+
+    response = await async_client.post(
+        "/api/v1/users/",
+        json={
+            "email": "beazley@example.com",
+            "password": "Str0ngP@ssw0rd!",
+            "firstName": "test_user",
+            "lastName": "test_user",
+        },
+    )
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.parametrize(
+    "email,password,first_name,last_name",
+    [
+        ("novaliddomain.ru", "password", None, None),
+        ("v@d.c", "password", "John", "Doe"),
+        ("valid@domain.com", "pass", "John", "Doe"),
+        ("valid@domain.com", "pass" * 100, "John", "Doe"),
+        ("valid@domain.com", " " * 10, "John", "Doe"),
+        ("valid@domain.com", "password", " " * 10, ""),
+        ("valid@domain.com", "password", "a", ""),
+        ("valid@domain.com", "password", "a" * 251, "Doe"),
+        ("valid@domain.com", "password", "John", " " * 10),
+        ("valid@domain.com", "password", "John", "a"),
+        ("valid@domain.com", "password", "", " " * 251),
+    ],
+)
+@pytest.mark.integration
+async def test_create_user_no_valid_payload__fail(
+    email: str,
+    password: str,
+    first_name: str | None,
+    last_name: str | None,
+    async_client: AsyncClient,
+) -> None:
+    response: Response = await async_client.post(
+        "/api/v1/users/",
+        json={
+            "email": email,
+            "password": password,
+            "firstName": first_name,
+            "lastName": last_name,
+        },
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
